bcc5f5725501a36470f3ce207bf36384c2f4de70
Ben Fok <benfok8@gmail.com>
2022-03-20 16:03:42 -0700

    updating model files

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true
+        }
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+          isNumeric: true
+        }
+      },
+      category_id: {
+        type: DataType.INTEGER,
+        // defines that this field is a reference to the category table and the column of id
+        references: {
+          model: 'category',
+          key: 'id'
+        }
+      }
   },
   {
     sequelize,
