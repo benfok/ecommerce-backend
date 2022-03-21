572a223ab8bf28cc866e0cbf12fac68a7861f1ea
Ben Fok <benfok8@gmail.com>
2022-03-21 16:06:20 -0700

    adding .env example file and adding some comments

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
-const db = require('./config/connection');
 // import sequelize connection
+const db = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
